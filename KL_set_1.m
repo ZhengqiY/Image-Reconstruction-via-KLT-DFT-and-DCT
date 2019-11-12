338cc4a555cbda501a1812cded3622a9b2a18cd7
ZhengqiY <51040140+ZhengqiY@users.noreply.github.com>
2019-11-12 15:55:48 -0800

    Add files via upload

---
+++ KL_set_1.m
@@ -0,0 +1,147 @@
+clear
+clc
+close all
+
+% Load the image set_1
+set_1 = dir('set_1');
+len = length(set_1);
+k = 0;
+for i = 3:len
+    filename = set_1(i).name;
+    img = imread(filename);
+    k = k + 1;
+    x(:,k) = img(:);
+end
+
+nImages = k;                     %total number of images
+imsize = size(img);       %size of image (they all should have the same size) 
+nPixels = imsize(1)*imsize(2);   %number of pixels in image
+x = double(x)/255;               %convert to double and normalize
+
+%Calculate the average
+avrgx = mean(x')';
+for i=1:nImages
+    x(:,i) = x(:,i) - avrgx; % substruct the average
+end
+%compute covariance matrix
+cov_mat = x'*x;
+[V,D] = eig(cov_mat);         %eigen values of cov matrix
+V = x*V*(abs(D))^-0.5;               
+ 
+%image decomposition coefficients
+KLCoef =  x'*V;
+
+%top 100 coefficients
+list100 = KLCoef(:);
+list100 = list100';
+sortl = sort(list100);
+gate = sortl(301);
+KLCoef100 = KLCoef;
+
+for i = 1:20
+    for j = 1:20
+        if KLCoef100(i,j) < gate
+            KLCoef100(i,j) = 0;
+        end
+    end
+end
+
+%top 20 coefficients
+list20 = KLCoef(:);
+list20 = list20';
+sortl = sort(list20);
+gate = sortl(381);
+KLCoef20 = KLCoef;
+
+for i = 1:20
+    for j = 1:20
+        if KLCoef20(i,j) < gate
+            KLCoef20(i,j) = 0;
+        end
+    end
+end
+
+%top 10 coefficients
+list10 = KLCoef(:);
+list10 = list10';
+sortl = sort(list10);
+gate = sortl(391);
+KLCoef10 = KLCoef;
+
+for i = 1:20
+    for j = 1:20
+        if KLCoef10(i,j) < gate
+            KLCoef10(i,j) = 0;
+        end
+    end
+end
+
+%top 5 coefficients
+list5 = KLCoef(:);
+list5 = list5';
+sortl = sort(list5);
+gate = sortl(396);
+KLCoef5 = KLCoef;
+
+for i = 1:20
+    for j = 1:20
+        if KLCoef5(i,j) < gate
+            KLCoef5(i,j) = 0;
+        end
+    end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% reconstruction of Image
+i = 1:20;  %index of image to be recontructed
+reconst = V*KLCoef';
+%  NOTE: The following for loop is generating 20 reconstructed images and saving
+%  these images so that I commentted this part in order to avoid repeating
+%  doing the same task each time.
+
+% % % for image_index = i
+% % %     
+% % %     diff = abs(reconst(:,image_index) - x(:,image_index));
+% % %     figure;
+% % %     subplot(1,2,1); imshow((reshape(avrgx+reconst(:,image_index), imsize))); title('Reconstructed');
+% % %     subplot(1,2,2); imshow((reshape(avrgx+x(:,image_index), imsize)));title('original');
+% % %     saveas(gcf, sprintf('reconst set 1 %d.jpg', image_index));
+% % %     close all
+% % % end
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% RMSE vs Top 100 KLCoef, Img 20
+reconst100 = V*KLCoef100';
+image_index = 20;
+diff = abs(reconst100(:,image_index) - x(:,image_index));
+rmse100 = sqrt(sum(diff.^2)/k);
+
+% RMSE vs Top 20 KLCoef, Img 20
+reconst20 = V*KLCoef20';
+image_index = 20;
+diff = abs(reconst20(:,image_index) - x(:,image_index));
+rmse20 = sqrt(sum(diff.^2)/k);
+
+% RMSE vs Top 10 KLCoef, Img 20
+reconst10 = V*KLCoef10';
+image_index = 20;
+diff = abs(reconst10(:,image_index) - x(:,image_index));
+rmse10 = sqrt(sum(diff.^2)/k);
+
+% RMSE vs Top 5 KLCoef, Img 20
+reconst5 = V*KLCoef5';
+image_index = 20;
+diff = abs(reconst5(:,image_index) - x(:,image_index));
+rmse5 = sqrt(sum(diff.^2)/k);
+
+figure
+subplot(2,2,1); imshow((reshape(avrgx+reconst100(:,image_index), imsize))); title('Reconstructed Top 100 Co');
+subplot(2,2,2); imshow((reshape(avrgx+reconst20(:,image_index), imsize))); title('Reconstructed Top 20 Co');
+subplot(2,2,3); imshow((reshape(avrgx+reconst10(:,image_index), imsize))); title('Reconstructed Top 10 Co');
+subplot(2,2,4); imshow((reshape(avrgx+reconst5(:,image_index), imsize))); title('Reconstructed Top 5 Co');
+
+%Plot RMSE vs KLCoef Num
+rmse_set_1 = [rmse100, rmse20, rmse10, rmse5];
+KLCoefnum = [100, 20, 10, 5];
+figure
+plot(KLCoefnum, rmse_set_1);
+title('Set 1 RMSE vs KLCoef Num');
---
+++ twoDDCT_set_2.m
@@ -0,0 +1,123 @@
+clear
+clc
+close all
+% Load the image set_2
+set_2 = dir('set_2');
+len = length(set_2);
+k = 0;
+for i = 3:len
+    filename = set_2(i).name;
+    img = imread(filename);
+    k = k + 1;
+    x(:,k) = img(:);
+end
+
+nImages = k;                     %total number of images
+imsize = size(img);       %size of image (they all should have the same size) 
+nPixels = imsize(1)*imsize(2);   %number of pixels in image
+x = double(x)/255;               %convert to double and normalize
+%Calculate the average
+avrgx = mean(x')';
+for i=1:nImages
+    x(:,i) = x(:,i) - avrgx; % substruct the average
+end
+
+image_index = 20; % Img
+
+image_2d = x(:,image_index);
+image_2d = reshape(image_2d+avrgx, imsize);
+
+A=dct2(image_2d);
+figure(1);
+imshow(abs(idct2(A)));title('2D-DCT Reconst');
+
+%top 10000 coefficients
+list100 = A(:);
+list100 = list100';
+sortl = sort(list100);
+gate = sortl(120*160-9999);
+A100 = A;
+
+for i = 1:120
+    for j = 1:160
+        if A100(i,j) < gate
+            A100(i,j) = 0;
+        end
+    end
+end
+
+figure(2);
+subplot(2,2,1); imshow(abs(idct2(A100))); title('Reconstructed Top 10000 Co');
+
+%top 7000 coefficients
+list20 = A(:);
+list20 = list20';
+sortl = sort(list20);
+gate = sortl(120*160-6999);
+A20 = A;
+
+for i = 1:120
+    for j = 1:160
+        if A20(i,j) < gate
+            A20(i,j) = 0;
+        end
+    end
+end
+subplot(2,2,2); imshow(abs(idct2(A20))); title('Reconstructed Top 7000 Co');
+
+%top 4000 coefficients
+list10 = A(:);
+list10 = list10';
+sortl = sort(list10);
+gate = sortl(120*160-3999);
+A10 = A;
+
+for i = 1:120
+    for j = 1:160
+        if A10(i,j) < gate
+            A10(i,j) = 0;
+        end
+    end
+end
+subplot(2,2,3); imshow(abs(idct2(A10))); title('Reconstructed Top 4000 Co');
+
+%top 1000 coefficients
+list5 = A(:);
+list5 = list5';
+sortl = sort(list5);
+gate = sortl(120*160-999);
+A5 = A;
+
+for i = 1:120
+    for j = 1:160
+        if A5(i,j) < gate
+            A5(i,j) = 0;
+        end
+    end
+end
+subplot(2,2,4); imshow(abs(idct2(A5))); title('Reconstructed Top 1000 Co');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% RMSE vs Top 10000 DCTCoef, Img 20
+diff = abs(double(abs(idct2(A100)) - image_2d));
+rmse100 = sqrt(sum(diff(:).^2)/k);
+
+% RMSE vs Top 7000 DCTCoef, Img 20
+diff = abs(double(abs(idct2(A20)) - image_2d));
+rmse20 = sqrt(sum(diff(:).^2)/k);
+
+% RMSE vs Top 4000 DCTCoef, Img 20
+diff = abs(double(abs(idct2(A10)) - image_2d));
+rmse10 = sqrt(sum(diff(:).^2)/k);
+
+% RMSE vs Top 1000 DCTCoef, Img 20
+diff = abs(double(abs(idct2(A5)) - image_2d));
+rmse5 = sqrt(sum(diff(:).^2)/k);
+
+%Plot RMSE vs DCTCoef Num
+rmse_set_2 = [rmse100, rmse20, rmse10, rmse5];
+DCTCoefnum = [10000, 7000, 4000, 1000];
+figure(3);
+plot(DCTCoefnum, rmse_set_2);
+title('Set 2 RMSE vs DCTCoef Num');
+
